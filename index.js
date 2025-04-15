decfebac13ebde83e4595242c5f8871031e2e7f3
unknown <juhbuscariollo026@gmail.com>
2025-04-15 10:34:49 -0300

    feito o exercicio 4

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
-// A constante abaixo armazenará a idade digitada pelo usuário
-const idade = parseFloat(prompt("Qual é a sua idade?"));
+ //A constante abaixo armazenará o número digitado pelo usuario
+const numeroPar = parseFloat(prompt("digite um número:"));
 
 // Verifica se o valor digitado é um número válido
-if (isNaN(idade)) {
+if (isNaN(numeroPar)) {
     alert("Erro: Você não digitou um número válido!");
 } else {
-    if (idade >= 18) {
-        // Se a idade for maior ou igual a 18, é maior de idade
-        alert("Você é maior de idade!");
-    } else if (idade >= 0) {
-        // Se a idade for entre 0 e 17, é menor de idade
-        alert("Você é menor de idade!");
+    if (numeroPar % 2 === 0) {
+        // Se o numero tiver o resto igual a 0, a resposta será par
+        alert("Seu número é par");
+
     } else {
-        // Se a idade for negativa, também é inválida
-        alert("Erro: Idade não pode ser negativa!");
+        // Se o numero não tiver resto de 0 em uma divisão, a resposta será impar
+        alert("Erro: seu número é impar");
     }
 }
-
-
