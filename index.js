62d2e62106b41a9212a2ff7badd719af88b5530b
unknown <juhbuscariollo026@gmail.com>
2025-04-15 12:35:57 -0300

    exercicio 4 concluido

--- index.js
+++ index.js
@@ -8,15 +8,16 @@ if (isNaN(valorCompra)) {
 } else {
 
         if (valorCompra<100) {
-        // Se o numero não tiver resto de 0 em uma divisão, a resposta será impar
+        // irá alertar caso a compra não tenha atingido o valor minimo
         alert (`sua compra não atingiu o valor minimo para ter direito á desconto, o valor da compra ficou em ${valorCompra}`)
 
         } else {
 
             const valorDesconto = valorCompra - (valorCompra * 0.10);
+            //irá demonstrar o valor que ficara a conta após a aplicação do desconto
             alert(`o valor da sua compra iria ficar R$ ${valorCompra}, mas após a aplicação do desconto, o valor da compra será ${valorDesconto.toFixed(2)}!`)
 
             }
 
         } 
-        
+    
